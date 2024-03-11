5a0cf199c8b4fc0245bc89039f261cdfc2e82be4
Lucasfs10 <139243449+Lucasfs10@users.noreply.github.com>
2024-03-11 13:41:29 -0300

    l

---
+++ Microsserviços/GeekShoppingAPI/Program.cs
@@ -0,0 +1,41 @@
+using Data.Models.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShoppingAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
+            builder.Services.AddDbContext<MySQLContext>(options => 
+            options.UseMySql(connection, new MySqlServerVersion(new Version(8, 3, 0))));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ Microsserviços/GeekShoppingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GeekShoppingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
